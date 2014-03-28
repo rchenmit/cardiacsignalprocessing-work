ffb79c87fe81de9e48ab48edea1c529baf6c6f06
Robert Chen <robchen401@gmail.com>
2014-03-28 01:21:26 -0400

    commit end of 3/27
    changed processing;
    conditioned SCG's before using kalman
    new plots for velocity gotten from SCG

--- play_20140325.m
+++ play_20140325.m
@@ -1,13 +1,26 @@
 %% 3/25/2014
+%% Robert Chen
+
 %REMEMBER TO ADD PATH FIRST!!!
-%hardware data
-ecgHW = readDeviceData('scgData/ecgExam.txt');
-scgSt = readDeviceData('scgData/scg1Exam.txt');
-scgPmi = readDeviceData('scgData/scg2Exam.txt');
+addpath('./')
+
+%directories for data - specify!
+hardwareDataDir = '../scgData/';
+echoDataDir = '../echoData/03-05-2014-Cardiac/';
+
+%read in the hardware data
+ecgHWfile = strcat(hardwareDataDir, 'ecgExam.txt')
+scgStFile = strcat(hardwareDataDir, 'scg1Exam.txt')
+scgPmiFile= strcat(hardwareDataDir, 'scg2Exam.txt')
+ecgHW = readDeviceData(ecgHWfile);
+scgSt = readDeviceData(scgStFile);
+scgPmi = readDeviceData(scgPmiFile);
 
 %echo data
-[echo headerEcho] = readEchoData('14-09-19.b8');
-[ecgEcho headerECG_Echo] = readEchoData('14-09-19.ecg');
+echoFile = strcat(echoDataDir, '14-09-19.b8')
+ecgEchoFile = strcat(echoDataDir, '14-09-19.ecg')
+[echo headerEcho] = readEchoData(echoFile);
+[ecgEcho headerECG_Echo] = readEchoData(ecgEchoFile);
 
 %play Bmode; terminate by ctrl-c
 playBmode(echo, 50);
@@ -22,7 +35,7 @@ plot(ecgHW(stIdx:6:enIdx))
 %cycles
 cycles_ecgHW = getCyclesFromEcg(ecgHW);
 figure
-plot(ecgHW);
+plot(ecgHW(stIdx:enIdx));
 hold on;
 scatter(cycles_ecgHW(:,1), ecgHW(cycles_ecgHW(:,1)), 'r*');
 
@@ -47,11 +60,12 @@ cycles_start_indexes = cycles_ecgHW(:,1);
 ecgHW_beatarray = [];
 scgSt_beatarray = [];
 scgPmi_beatarray = [];
-
+num_samples_700ms = floor(1200*.7);
+num_samples_avgCycle = mean(cycles_ecgHW(:,3));
 for i = 1:length(cycles_start_indexes)
-    this_ecg_700 = ecgHW(cycles_start_indexes(i):cycles_start_indexes(i)+700);
-    this_scgSt_700 = scgSt(cycles_start_indexes(i):cycles_start_indexes(i)+700);
-    this_scgPmi_700 = scgPmi(cycles_start_indexes(i):cycles_start_indexes(i)+700);
+    this_ecg_700 = ecgHW(cycles_start_indexes(i):cycles_start_indexes(i)+num_samples_700ms);
+    this_scgSt_700 = scgSt(cycles_start_indexes(i):cycles_start_indexes(i)+num_samples_700ms);
+    this_scgPmi_700 = scgPmi(cycles_start_indexes(i):cycles_start_indexes(i)+num_samples_700ms);
     ecgHW_beatarray = [ecgHW_beatarray, this_ecg_700];
     scgSt_beatarray = [scgSt_beatarray, this_scgSt_700];
     scgPmi_beatarray = [scgPmi_beatarray, this_scgPmi_700];
@@ -60,13 +74,26 @@ ecgHW_EA = mean(ecgHW_beatarray,2);
 scgSt_EA = mean(scgSt_beatarray, 2);
 scgPmi_EA = mean(scgPmi_beatarray, 2);
 
+%condition signals
+%%%do this on ecgEcho as well!
+cond_ecgHW_EA = conditionDeviceEcg(ecgHW_EA);
+cond_scgSt_EA = conditionDeviceScg(scgSt_EA);
+cond_scgPmi_EA = conditionDeviceScg(scgPmi_EA);
+
 % find the velocity from the acceleration (SCG)
-kalm_scgSt = kalm(scgSt_EA);
-kalm_scgPmi = kalm(scgPmi_EA);
+kalm_scgSt = kalm(cond_scgSt_EA);
+kalm_scgPmi = kalm(cond_scgPmi_EA);
 scgSt_position = kalm_scgSt(1,:);
 scgSt_velocity = kalm_scgSt(2,:);
 scgPmi_position = kalm_scgPmi(1,:);
 scgPmi_velocity = kalm_scgPmi(2,:);
+
+figure
+hold on
+plot(cond_ecgHW_EA, '-b')
+plot(cond_scgSt_EA, '-r')
+plot(cond_scgPmi_EA, '-g')
+legend('ecgHW\_EA', 'scgSt\_EA', 'scgPmi\_EA')
 
 figure
 hold on
@@ -83,4 +110,6 @@ title('velocity ensemble average over time')
 legend('scgSt\_velocity', 'scgPmi\_velocity')
 
 
+%% save
+save('play_20140325.mat')
 
