77f41ebbadc4985fe8a65a9c80f41e5a2d4c4f70
rchenmit <robchen401@gmail.com>
2014-04-01 14:29:45 -0400

    changed ensemble average code

--- ensemble_average.m
+++ ensemble_average.m
@@ -1,14 +1,21 @@
-function ensemble_avg = ensemble_average(signal_data, cycles_start_indexes, fs, num_seconds_to_sample)
+function ensemble_avg = ensemble_average(signal_data, cycles_start_indexes, fs, num_seconds_backward_sample, num_seconds_forward_sample)
     %calculate ensemble average
     %INPUT PARAMETERS: 
     %    signal_data:
     %    start_indexes: list of start indexes (in the signal_data) 
+    %    fs: sampling freq
+    %    num_seconds_backward_sample: number of seconds to sample BEHIND
+    %    the peak
+    %    num_seconds_forward_sample: number of seconds to sample FORWARD
+    %    from the peak
     %OUTPUT: 
     %    ensemble_average : ensemble average of all individual beats
-    num_samples = floor(fs*num_seconds_to_sample);
+    num_samples_backward = floor(fs*num_seconds_backward_sample);
+    num_samples_forward = floor(fs*num_seconds_forward_sample);
+    
     beatarray = [];
     for i = 1:length(cycles_start_indexes)
-        this_signal = signal_data(cycles_start_indexes(i):cycles_start_indexes(i)+ num_samples);
+        this_signal = signal_data(cycles_start_indexes(i)-num_samples_backward:cycles_start_indexes(i)+ num_samples_forward);
         beatarray = [beatarray, this_signal];
     end
     
